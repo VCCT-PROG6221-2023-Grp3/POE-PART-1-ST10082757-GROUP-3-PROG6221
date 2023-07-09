09c6f1cccd93cc5d0a37ee0dc191267a930db66d
ST10082757 <ST10082757@vcconnect.edu.za>
2023-07-09 23:12:17 +0200

    used a delegate to notify user of exceeding 300 calories

--- POE PART 1 ST10082757 GROUP 3 PROG6221/COOKBOOK.cs
+++ POE PART 1 ST10082757 GROUP 3 PROG6221/COOKBOOK.cs
@@ -15,7 +15,7 @@ namespace POE_PART_2_ST10082757_GROUP_3_PROG6221
     //delegate for notification of calories exceeding 300
     public delegate void RecipeNotificationHandler(string message);
     public class COOKBOOK 
-    {
+    { 
 
         //variables and lists needed for this class
         private string RecipeName;
